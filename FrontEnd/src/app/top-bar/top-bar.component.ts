f7df369d801b2cb5455f148c37e0b62884ec49c8
Dennes Torres <dennes@bufaloinfo.com.br>
2022-01-31 14:51:44 +0000

    First topbar version

---
+++ FrontEnd/src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'topbar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
